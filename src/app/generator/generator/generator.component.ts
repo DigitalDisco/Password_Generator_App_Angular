aa31dc030946364f392dc2a25ded8b3cd9e7ad54
Atlas Sandevi <62032234+DigitalDisco@users.noreply.github.com>
2023-05-19 19:38:17 +0200

    Add files via upload

--- src/app/generator/generator/generator.component.ts
+++ src/app/generator/generator/generator.component.ts
@@ -8,10 +8,10 @@ import { environment } from "src/environments/environment";
 })
 export class GeneratorComponent implements OnInit {
 	@ViewChild("clickToCopy") clickToCopy!: ElementRef;
-	@ViewChild("copiado") copiado!: ElementRef;
-	semilla_1: string = environment.semilla_1;
+	@ViewChild("copy") copy!: ElementRef;
+	similarity_1: string = environment.similarity_1;
 	lengthValue: number = 0;
-	mayusculas: string[] = [
+	capitals: string[] = [
 		"A",
 		"B",
 		"C",
@@ -40,7 +40,7 @@ export class GeneratorComponent implements OnInit {
 		"Y",
 		"Z",
 	];
-	minusculas: string[] = [
+	lowercase: string[] = [
 		"a",
 		"b",
 		"c",
@@ -69,8 +69,8 @@ export class GeneratorComponent implements OnInit {
 		"y",
 		"z",
 	];
-	numeros: string[] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "1"];
-	simbolos: string[] = [
+	number: string[] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "1"];
+	symbols: string[] = [
 		"!",
 		"@",
 		"#",
@@ -95,7 +95,7 @@ export class GeneratorComponent implements OnInit {
 		"¨",
 		"·",
 	];
-	options: string[] = ["mayusculas", "minusculas", "numeros"];
+	options: string[] = ["capitals", "lowercase", "number"];
 	optionsGenerate(typo: string) {
 		if (!!this.options.find((option) => option === typo)) {
 			this.options = this.options.filter((option) => option !== typo);
@@ -109,43 +109,43 @@ export class GeneratorComponent implements OnInit {
 	}
 	functions: any = {
 		mayus: () => {
-			let rand = Math.floor(Math.random() * this.mayusculas.length);
-			let nv: any = this.semilla_1.split("");
+			let rand = Math.floor(Math.random() * this.capitals.length);
+			let nv: any = this.similarity_1.split("");
 			if (rand < nv.length) {
-				let res = (nv[rand] + this.mayusculas.length) % this.mayusculas.length;
-				return this.mayusculas[res];
+				let res = (nv[rand] + this.capitals.length) % this.capitals.length;
+				return this.capitals[res];
 			} else {
-				return this.mayusculas[rand];
+				return this.capitals[rand];
 			}
 		},
 		minus: () => {
-			let rand = Math.floor(Math.random() * this.minusculas.length);
-			let nv: any = this.semilla_1.split("");
+			let rand = Math.floor(Math.random() * this.lowercase.length);
+			let nv: any = this.similarity_1.split("");
 			if (rand < nv.length) {
-				let res = (nv[rand] + this.minusculas.length) % this.minusculas.length;
-				return this.minusculas[res];
+				let res = (nv[rand] + this.lowercase.length) % this.lowercase.length;
+				return this.lowercase[res];
 			} else {
-				return this.minusculas[rand];
+				return this.lowercase[rand];
 			}
 		},
 		nums: () => {
-			let rand = Math.floor(Math.random() * this.numeros.length);
-			let nv: any = this.semilla_1.split("");
+			let rand = Math.floor(Math.random() * this.number.length);
+			let nv: any = this.similarity_1.split("");
 			if (rand < nv.length) {
-				let res = (nv[rand] + this.numeros.length) % this.numeros.length;
-				return this.numeros[res];
+				let res = (nv[rand] + this.number.length) % this.number.length;
+				return this.number[res];
 			} else {
-				return this.numeros[rand];
+				return this.number[rand];
 			}
 		},
 		simb: () => {
-			let rand = Math.floor(Math.random() * this.simbolos.length);
-			let nv: any = this.semilla_1.split("");
+			let rand = Math.floor(Math.random() * this.symbols.length);
+			let nv: any = this.similarity_1.split("");
 			if (rand < nv.length) {
-				let res = (nv[rand] + this.simbolos.length) % this.simbolos.length;
-				return this.simbolos[res];
+				let res = (nv[rand] + this.symbols.length) % this.symbols.length;
+				return this.symbols[res];
 			} else {
-				return this.simbolos[rand];
+				return this.symbols[rand];
 			}
 		},
 	};
@@ -153,55 +153,55 @@ export class GeneratorComponent implements OnInit {
 		document.addEventListener("copy", (e: ClipboardEvent) => {
 			this.clickToCopy.nativeElement.style =
 				"transform: translateY(200%); opacity: 0;";
-			this.copiado.nativeElement.style =
+			this.copy.nativeElement.style =
 				"transform: translateY(0%); opacity: 0.5;";
 			if (e.clipboardData) {
-				e.clipboardData.setData("text/plain", this.password_generada);
+				e.clipboardData.setData("text/plain", this.password_generate);
 				e.preventDefault();
 			}
 		});
 		document.execCommand("copy");
 	}
 	nvClass = "";
-	password_generada: string = "";
+	password_generate: string = "";
 	generatePassword(): void {
 		this.nvClass = "actt";
-		this.copiado.nativeElement.style = "transform: translateY(200%); opacity: 0;";
+		this.copy.nativeElement.style = "transform: translateY(200%); opacity: 0;";
 		this.clickToCopy.nativeElement.style =
 			"transform: translateY(0%); opacity: 0.5;";
-		this.password_generada = "";
-		let find_mayusculas = !!this.options.find(
-			(option) => option === "mayusculas"
+		this.password_generate = "";
+		let find_capitals = !!this.options.find(
+			(option) => option === "capitals"
 		);
-		let find_minusculas = !!this.options.find(
-			(option) => option === "minusculas"
+		let find_lowercase = !!this.options.find(
+			(option) => option === "lowercase"
 		);
-		let find_numeros = !!this.options.find((option) => option === "numeros");
-		let find_simbolos = !!this.options.find((option) => option === "simbolos");
-		if (!find_numeros && !find_simbolos && !find_mayusculas && !find_minusculas) {
+		let find_number = !!this.options.find((option) => option === "number");
+		let find_symbols = !!this.options.find((option) => option === "symbols");
+		if (!find_number && !find_symbols && !find_capitals && !find_lowercase) {
 			alert("Select at least one setting option");
-			throw new Error("No seleccionaste ninguna opcion");
+			throw new Error("You have not selected at least one option");
 		}
 		let j = 0;
 		for (let i = 0; i < this.lengthValue; i++) {
 			let rand = Math.floor(Math.random() * 4);
-			if (rand == 0 && find_mayusculas) {
-				this.password_generada += this.functions.mayus();
+			if (rand == 0 && find_capitals) {
+				this.password_generate += this.functions.mayus();
 				j = 0;
-			} else if (rand == 1 && find_minusculas) {
-				this.password_generada += this.functions.minus();
+			} else if (rand == 1 && find_lowercase) {
+				this.password_generate += this.functions.minus();
 				j = 1;
-			} else if (rand == 2 && find_numeros) {
-				this.password_generada += this.functions.nums();
+			} else if (rand == 2 && find_number) {
+				this.password_generate += this.functions.nums();
 				j = 2;
-			} else if (rand == 3 && find_simbolos) {
-				this.password_generada += this.functions.simb();
+			} else if (rand == 3 && find_symbols) {
+				this.password_generate += this.functions.simb();
 				j = 3;
 			} else {
 				i--;
 			}
 		}
-		console.log(this.password_generada.length);
+		console.log(this.password_generate.length);
 	}
 	ngOnInit(): void {
 		this.lengthValue = 16;
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
 	production: false,
-	semilla_1: "2638859338349779227127935153573637955673",
+	similarity_1: "2638859338349779227127935153573637955673",
 };
 
 /*
